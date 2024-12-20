58a83bd89a52c90684868dde24b9e1e98fad230d
Sudeepa-R <sudeepar084@gmail.com>
2024-12-20 07:52:46 +0530

    dev ui issue cleared

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // Removed ThemeProvider import due to unresolved import error
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <App />
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
