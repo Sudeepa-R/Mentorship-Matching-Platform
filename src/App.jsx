214024d8b9ad3e40dddd89e004d0a5ea9a986aff
Sudeepa R <119294212+Sudeepa-R@users.noreply.github.com>
2024-12-21 12:38:01 +0530

    Revert "Merge branch 'dev' of https://github.com/Sudeepa-R/Mentorship-Matchin…"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,55 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import RouterProvider from './constants/RoutesProvider'
+import LoginPage from './constants/LoginPage/loginPage'
+import Login from './constants/LoginPage/Example'
+import ForgotPass from './constants/LoginPage/ForgotPass'
 
 function App() {
+  const [count, setCount] = useState(0)
 
+=======
+import "./App.css";
+import RouterProvider from "./constants/RoutesProvider";
+
+function App() {
+>>>>>>> be8bcddf2e6a08e445d8894e8cfad51e26efb244
+=======
+import "./App.css";
+import RouterProvider from "./constants/RoutesProvider";
+
+function App() {
+>>>>>>> be8bcddf2e6a08e445d8894e8cfad51e26efb244
+=======
+import "./App.css";
+import RouterProvider from "./constants/RoutesProvider";
+
+function App() {
+>>>>>>> be8bcddf2e6a08e445d8894e8cfad51e26efb244
   return (
     <>
-      <RouterProvider />
+      <LoginPage/>
+      {/* <ForgotPass/> */}
+      {/* <Login/> */}
     </>
-
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
   )
+=======
+  );
+>>>>>>> be8bcddf2e6a08e445d8894e8cfad51e26efb244
+=======
+  );
+>>>>>>> be8bcddf2e6a08e445d8894e8cfad51e26efb244
+=======
+  );
+>>>>>>> be8bcddf2e6a08e445d8894e8cfad51e26efb244
 }
 
-export default App
+export default App;
+export default App;
--- src/constants/LoginPage/loginPage.jsx
+++ src/constants/LoginPage/loginPage.jsx
@@ -10,6 +10,14 @@ import {
 } from "@ant-design/icons";
 import "./Logingpage.scss";
 import ForgotPass from "./ForgotPass";
+import { showMessage, showNotification } from "../Toaster/toaster";
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+=======
+>>>>>>> be8bcddf2e6a08e445d8894e8cfad51e26efb244
+=======
+>>>>>>> be8bcddf2e6a08e445d8894e8cfad51e26efb244
 
 export class LoginPage extends React.Component {
   constructor(props){
@@ -27,24 +35,6 @@ export class LoginPage extends React.Component {
   handleClose=()=>{
     this.setState({forgotPass:false})
   }
-
-  handleLogin = (values) => {
-    const { username, password } = values;
-
-    if (username === "test@example.com" && password === "password123") {
-      showMessage("success", "Login successful!");
-    } else {
-      showMessage("error", "Invalid username or password!");
-    }
-  };
-
-  handleLoginFailed = (errorInfo) => {
-    showNotification({
-      type: "warning",
-      title: "Form Submission Failed",
-      description: "Please check the form fields and try again.",
-    });
-  };
 
   handleLogin = (values) => {
     const { username, password } = values;
@@ -118,7 +108,8 @@ export class LoginPage extends React.Component {
           >
             <img
               src={loginImg}
-              alt="loging page image"
+              alt="login page image"
+              alt="login page image"
               style={{ width: "100%", height: "100%" }}
             />
           </Col>
@@ -151,10 +142,15 @@ export class LoginPage extends React.Component {
                   style={{
                     minWidth: 350,
                   }}
+                  onFinish={this.handleLogin}
+                  onFinishFailed={this.handleLoginFailed}
+                  onFinish={this.handleLogin}
+                  onFinishFailed={this.handleLoginFailed}
                   // onFinish={onFinish}
                 >
                   <p>
-                    Enter your email address and password to access you portal!
+                    Enter your email address and password to access your portal!
+                    Enter your email address and password to access your portal!
                   </p>
                   <Form.Item
                     label="Username"
@@ -241,6 +237,8 @@ export class LoginPage extends React.Component {
             // onOk={this.handleOnOk()}
             onCancel={() => this.handleClose()}
             footer={false}
+            className=".forget-model"
+            className=".forget-model"
           >
             <ForgotPass />
           </Modal>
@@ -252,3 +250,4 @@ export class LoginPage extends React.Component {
 }
 
 export default LoginPage;
+
