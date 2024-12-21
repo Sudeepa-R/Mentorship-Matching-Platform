590a523545d63b373076472fd8eb2200c8634c0a
spexzee <abub5979@gmail.com>
2024-12-21 20:27:35 +0530

    minor change in dark theme

--- src/constants/home/Home.jsx
+++ src/constants/home/Home.jsx
@@ -1,11 +1,12 @@
-import React, { useRef,useEffect, useContext } from "react";
+import React, { useRef, useEffect, useContext } from "react";
 import { Box, Typography, Button } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import "./home.scss";
-import {get} from '../../api/API'
+import { get } from '../../api/API'
 import ThemeContext from "../../context/theme/ThemeContext";
+import { plans, stories } from "../../data";
 
 function Home() {
   const navigate = useNavigate();
@@ -27,8 +28,8 @@ function Home() {
     console.log('Component is about to mount');
 
     // Optionally clean up if needed
-   
-}, []); 
+
+  }, []);
   const sectionRefs = {
     home: homeRef,
     findMentor: findMentorRef,
@@ -39,8 +40,8 @@ function Home() {
 
   const backendTest = async () => {
     const test = await get('/users/1');
-    console.log(test);  
-};
+    console.log(test);
+  };
 
   return (
     <>
@@ -98,31 +99,9 @@ function Home() {
           and unlock new opportunities.
         </Typography>
 
-       
+
         <Box className="cardContainer">
-          {[
-            {
-              image:
-                "https://img.freepik.com/free-vector/3d-cartoon-people-concept-online-meeting-virtual-conference-video-call_40876-3763.jpg?t=st=1734586121~exp=1734589721~hmac=4ba87f4f92998bcd4b2e739c142939f95eef8dbe4b98f5e022e8b1b04dd69cd5&w=900",
-              title: "Story 1",
-              content:
-                "Learn more about how our platform can help you connect with mentors and unlock new opportunities.",
-            },
-            {
-              image:
-                "https://img.freepik.com/free-vector/3d-cartoon-people-concept-online-meeting-virtual-conference-video-call_40876-3763.jpg?t=st=1734586121~exp=1734589721~hmac=4ba87f4f92998bcd4b2e739c142939f95eef8dbe4b98f5e022e8b1b04dd69cd5&w=900",
-              title: "Story 2",
-              content:
-                "Learn more about how our platform can help you connect with mentors and unlock new opportunities.",
-            },
-            {
-              image:
-                "https://img.freepik.com/free-vector/3d-cartoon-people-concept-online-meeting-virtual-conference-video-call_40876-3763.jpg?t=st=1734586121~exp=1734589721~hmac=4ba87f4f92998bcd4b2e739c142939f95eef8dbe4b98f5e022e8b1b04dd69cd5&w=900",
-              title: "Story 3",
-              content:
-                "Learn more about how our platform can help you connect with mentors and unlock new opportunities.",
-            },
-          ].map((card, index) => (
+          {stories.map((card, index) => (
             <Box key={index} className="card">
               <img src={card.image} alt={card.title} className="cardImage" />
               <Typography variant="h6" className="cardTitle">
@@ -151,71 +130,62 @@ function Home() {
         </Box>
       </Box>
 
-       {/*---- Membership Plans Section---------- */}
-     
+      {/*---- Membership Plans Section---------- */}
 
-      
       <Box ref={membershipPlansRef} id="membershipPlans" className="section-3" style={isDark ? { backgroundColor: '#444', color: '#fff' } : {}}>
         <Typography variant="h3" className="sectionTitle-3">
           Membership Plans
         </Typography>
-       
+
         <Box className="cardContainer-3">
-    {[
-      { title: 'Unlimited', content: 'Learn more about how our platform can help you connect with mentors and unlock new opportunities.Learn more about how our platform can help you connect with mentors and unlock new opportunities.Learn more about how our platform can help you connect with mentors and unlock new opportunities.', subHeading: 'Credits $12/months' },
-      { title: 'Pro', content: 'Learn more about how our platform can help you connect with mentors and unlock new opportunities.Learn more about how our platform can help you connect with mentors and unlock new opportunities.Learn more about how our platform can help you connect with mentors and unlock new opportunities.', subHeading: 'Credits $12/months' },
-      { title: 'Free', content: 'Learn more about how our platform can help you connect with mentors and unlock new opportunities.Learn more about how our platform can help you connect with mentors and unlock new opportunities.Learn more about how our platform can help you connect with mentors and unlock new opportunities.', subHeading: 'Free' },
-    ].map((card2, index) => (
-      <Box key={index} className="card2" sx={{ textAlign: 'left', padding: '16px', margin: '16px', border: '1px solid #ddd', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', backgroundColor: isDark ? '#555' : '#fff', color: isDark ? '#fff' : '#000' }}>
-        <Typography variant="h5" className="cardTitle2" sx={{ fontWeight: 'bold', marginBottom: '8px' }}>
-          {card2.title}
-        </Typography>
-        <Typography variant="body1" className="cardContent2" sx={{ marginBottom: '8px' }}>
-          {card2.content}
-        </Typography>
-        <Typography variant="subtitle1" sx={{ marginBottom: '16px' }}>
-          {card2.subHeading}
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          className="cardButton2"
-          sx={{
-            margin: '1rem auto',
-            display: 'block',
-            backgroundColor: isDark ? '#fff' : '#fff',
-            color: isDark ? '#537786' : '#537786',
-            textTransform: 'capitalize',
-            border: '',
-            borderRadius: '0', 
-          }}
-          
-          onClick={() => navigate("/login")}
-        >
-          Purchase
-        </Button>
-      </Box>
-    ))}
-  </Box>
-      </Box>
-       {/*----------- ContactUs-------------- */}
-       
-       <Box
-      ref={contactUsRef}
-      id="contactUs"
-      className="section-4"
-      to="/contactUs"
-      style={isDark ? { backgroundColor: '#444', color: '#fff' } : {}}
-    >
-      <Typography variant="h3" className="sectionTitle-4">
-    Contact Us
-  </Typography>
-      <Footer />
-      
-    </Box>
-    
+          {plans.map((card2, index) => (
+            <Box key={index} className="card2" sx={{ textAlign: 'left', padding: '16px', margin: '16px', border: '1px solid #ddd', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+              <Typography variant="h5" className="cardTitle2" sx={{ fontWeight: 'bold', marginBottom: '8px' }}>
+                {card2.title}
+              </Typography>
+              <Typography variant="body1" className="cardContent2" sx={{ marginBottom: '8px' }}>
+                {card2.content}
+              </Typography>
+              <Typography variant="subtitle1" sx={{ marginBottom: '16px' }}>
+                {card2.subHeading}
+              </Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                className="cardButton2"
+                sx={{
+                  margin: '1rem auto',
+                  display: 'block',
+                  backgroundColor: isDark ? '#fff' : '#fff',
+                  color: isDark ? '#537786' : '#537786',
+                  textTransform: 'capitalize',
+                  border: '',
+                  borderRadius: '0',
+                }}
 
+                onClick={() => navigate("/login")}
+              >
+                Purchase
+              </Button>
+            </Box>
+          ))}
+        </Box>
+      </Box>
+      {/*----------- ContactUs-------------- */}
 
+      <Box
+        ref={contactUsRef}
+        id="contactUs"
+        className="section-4"
+        to="/contactUs"
+        style={isDark ? { backgroundColor: '#444', color: '#fff' } : {}}
+      >
+        <Typography variant="h3" className="sectionTitle-4">
+          Contact Us
+        </Typography>
+        <Footer />
+
+      </Box>
     </>
   );
 }
