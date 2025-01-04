7d724af9dc3233816f2ea93571cf57e960f7e27c
Sudeepa-R <sudeepar084@gmail.com>
2025-01-04 22:26:43 +0530

    username is verified based on the condition

--- src/constants/commonFuncions.js
+++ src/constants/commonFuncions.js
@@ -1,6 +1,17 @@
 const baseURL = import.meta.env.VITE_MMP_BACKEND_URL;
 const success = 200;
 
-const commonFunction = { baseURL, success };
+const isValidPhoneNumber = (phoneNumber) => {
+  const phoneRegex = /^[+]?[0-9]{10,15}$/;
+  const res = phoneRegex.test(phoneNumber);
+  return res;
+};
+
+const isValidEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+const commonFunction = { baseURL, success, isValidPhoneNumber, isValidEmail };
 
 export default commonFunction;
